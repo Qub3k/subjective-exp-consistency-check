0ebcb18e482759b80b0831c8471b63d5137ddda9
Jakub Nawała <jakub.tadeusz.nawala@gmail.com>
2021-01-14 16:48:38 +0100

    Create a stub of the master reproducibility script

---
+++ reproduce.py
@@ -0,0 +1,20 @@
+# A master script allowing to reproduce the results from the following ACM MM'20 paper:
+#
+# @inproceedings{Nawała2020,
+#     author = {Nawa{\l}a, Jakub and Janowski, Lucjan and {\'{C}}miel, Bogdan and Rusek, Krzysztof},
+#     booktitle = {Proceedings of the 28th ACM International Conference on Multimedia (MM ’20)},
+#     keywords = {experiment consistency,experiment validity,quality of experience,subjective experiment},
+#     title = {{Describing Subjective Experiment Consistency by p-Value P–P Plot}},
+#     year = {2020}
+# }
+#
+# Author: Jakub Nawała <jnawala@agh.edu.pl>
+# Created: Jan 14, 2021
+
+def main():
+    # TODO 1. Reproduce the creation of Tab. 1
+    return
+
+
+if __name__ == '__main__':
+    exit(0)
